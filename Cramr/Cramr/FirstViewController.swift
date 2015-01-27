74aeba0a887fb720104d3d7dee205c77406ce075
Anton Apostolatos <antonaf@stanford.edu>
2015-01-26 22:40:08 -0800

    Adding Cramr version 2.0

---
+++ Cramr/Cramr/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Cramr
+//
+//  Created by Anton Apostolatos on 1/26/15.
+//  Copyright (c) 2015 Casa, Inc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
