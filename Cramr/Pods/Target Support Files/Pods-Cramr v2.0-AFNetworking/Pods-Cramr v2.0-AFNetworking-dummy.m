129ede95f96e4da7c6813842dc7f6e122e419d93
Anton Apostolatos <antonaf@stanford.edu>
2015-02-19 14:14:36 -0800

    Clean up round 2.

--- Cramr/Pods/Target Support Files/Pods-Cramr v2.0-AFNetworking/Pods-Cramr v2.0-AFNetworking-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Cramr_v2_0_AFNetworking : NSObject
-@end
-@implementation PodsDummy_Pods_Cramr_v2_0_AFNetworking
-@end
--- Cramr/Pods/Target Support Files/Pods-Cramr v2.0-Facebook-iOS-SDK/Pods-Cramr v2.0-Facebook-iOS-SDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Cramr_v2_0_Facebook_iOS_SDK : NSObject
-@end
-@implementation PodsDummy_Pods_Cramr_v2_0_Facebook_iOS_SDK
-@end
